2deb2110d81154d2db01fed8c284db1c2a4b533d
Kory <korymbuck@gmail.com>
2025-09-04 11:18:05 -0500

    Added other users route maps to latest run card in Social Page

--- src/App.vue
+++ src/App.vue
@@ -1806,7 +1806,7 @@ onMounted(() => {
                       )
                     }}
                   </p>
-                  <!-- MAP CONTAINER -->
+                  <!-- MAP CONTAINER-->
                   <div
                     v-if="
                       friend.stats?.lastRun?.route &&
